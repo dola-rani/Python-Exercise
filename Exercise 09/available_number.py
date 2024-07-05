e238f599e3c01dc453c3f6d39d3c606feb814935
DOLA RANI MONDAL <66483589+dola-rani@users.noreply.github.com>
2024-07-05 21:54:46 +0600

    Update available_number.py

--- Exercise 09/available_number.py
+++ Exercise 09/available_number.py
@@ -1,8 +1,9 @@
 '''09: Find if 6 available in the list [‘4, 8, 7, 4,3,6,2,1,9’]'''
 
-numbers_list = ['4', '8', '7', '4', '3', '6', '2', '1', '9']
+numbers = [4, 8, 7, 4, 3, 6, 2, 1, 9]
 
-if '6' in numbers_list:
+if numbers.count(6) > 0:
     print("6 is available in the list")
 else:
     print("6 is not available in the list")
+
